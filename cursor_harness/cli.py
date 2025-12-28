b35ea0f5da3379f9f631facc3679daf2aa76086a
Nirmal Arya <hello@nirmalarya.com>
2025-12-27 19:33:58 -0500

    fix: Add --version flag

--- cursor_harness/cli.py
+++ cursor_harness/cli.py
@@ -11,6 +11,7 @@ def main():
     parser = argparse.ArgumentParser(
         description='cursor-harness v3.0 - Simple, reliable autonomous coding'
     )
+    parser.add_argument('--version', action='version', version='cursor-harness v3.0.0-beta')
     
     subparsers = parser.add_subparsers(dest='mode', help='Mode')
     
